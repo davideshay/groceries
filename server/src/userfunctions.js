25ac274f3f7d986c84f7580c9e0756a961fcef39
David Shay <davideshay@gmail.com>
2023-03-16 16:57:49 -0400

    Added error checking to backend.

--- server/src/userfunctions.js
+++ server/src/userfunctions.js
@@ -39,7 +39,7 @@ let todosDBAsAdmin;
 let usersDBAsAdmin;
 const _ = require('lodash');
 const { v4: uuidv4 } = require('uuid');
-const { cloneDeep, isEmpty, isEqual } = require('lodash');
+const { cloneDeep, isEmpty, isEqual, isSafeInteger } = require('lodash');
 const {  emailPatternValidation, usernamePatternValidation, fullnamePatternValidation, uomContent } = require('./utilities');
 let uomContentVersion = 0;
 const targetUomContentVersion = 2;
@@ -266,7 +266,9 @@ async function addStockedAtIndicatorToSchema() {
     console.log("STATUS: Upgrading schema to support stocked at indicators.");
     const itemq = { selector: { type: { "$eq": "item"}},
                     limit: await totalDocCount(todosDBAsAdmin)};
-    let foundItemDocs = await todosDBAsAdmin.find(itemq);
+    let foundItemDocs;
+    try {foundItemDocs = await todosDBAsAdmin.find(itemq);}                
+    catch(err) {console.log("ERROR: Could not find item docs to update during schema update"); return false;}
     console.log("STATUS: Found items to update :", foundItemDocs.docs.length)
     for (let i = 0; i < foundItemDocs.docs.length; i++) {
         const foundItemDoc = foundItemDocs.docs[i];
@@ -298,7 +300,9 @@ async function restructureListGroupSchema() {
     console.log("STATUS: Upgrading lists");
     const listq = { selector: { type: { "$eq": "list"}},
                     limit: await totalDocCount(todosDBAsAdmin)};
-    let foundListDocs = await todosDBAsAdmin.find(listq);
+    let foundListDocs;
+    try {foundListDocs = await todosDBAsAdmin.find(listq);}
+    catch (err) {console.log("ERROR: Could not find lists to update:",err); return false;}
     console.log("STATUS: Found lists to update: ", foundListDocs.docs.length);
     for (let i = 0; i < foundListDocs.docs.length; i++) {
         const foundListDoc = foundListDocs.docs[i];
@@ -312,7 +316,9 @@ async function restructureListGroupSchema() {
     }
     const itemq = { selector: { type: { "$eq": "item"}},
                     limit: await totalDocCount(todosDBAsAdmin)};
-    let foundItemDocs = await todosDBAsAdmin.find(itemq);
+    let foundItemDocs;                
+    try {foundItemDocs = await todosDBAsAdmin.find(itemq);}
+    catch(err) {console.log("ERROR: Could not find lists to update during schema update:",err); return false;}
     console.log("STATUS: Found items to update :", foundItemDocs.docs.length)
     for (let i = 0; i < foundItemDocs.docs.length; i++) {
         const foundItemDoc = foundItemDocs.docs[i];
@@ -325,7 +331,9 @@ async function restructureListGroupSchema() {
     }
     console.log("STATUS: Creating default listgroups for all users");
     const userq = { selector: { type: "user", name: {$exists: true}}, limit: await totalDocCount(usersDBAsAdmin)};
-    let foundUserDocs = await usersDBAsAdmin.find(userq);
+    let foundUserDocs;
+    try {foundUserDocs = await usersDBAsAdmin.find(userq);}
+    catch(err) {console.log("ERROR: Could not find user list during schema update:",err); return false;}
     console.log("STATUS: Found users to create listgroups: ", foundUserDocs.docs.length);
     for (let i = 0; i < foundUserDocs.docs.length; i++) {
         const foundUserDoc = foundUserDocs.docs[i];
@@ -355,7 +363,9 @@ async function setSchemaVersion(updSchemaVersion) {
     const dbidq = {
         selector: { type: { "$eq": "dbuuid" }}
     }
-    let foundIDDocs =  await todosDBAsAdmin.find(dbidq);
+    let foundIDDocs;
+    try {foundIDDocs =  await todosDBAsAdmin.find(dbidq);}
+    catch (err) {console.log("ERROR: Couldn't find DBUUID record"); return false;}
     let foundIDDoc = undefined;
     if (foundIDDocs.docs.length > 0) {foundIDDoc = foundIDDocs.docs[0]}
     if (foundIDDoc == undefined) {
@@ -416,8 +426,10 @@ async function dbStartup() {
     console.log("STATUS: Access token expires in ",accessTokenExpires);
     await createDBIfNotExists();
     await setDBSecurity();
-    todosDBAsAdmin = todosNanoAsAdmin.use(couchDatabase);
-    usersDBAsAdmin = usersNanoAsAdmin.use("_users");
+    try {todosDBAsAdmin = todosNanoAsAdmin.use(couchDatabase);}
+    catch(err) {console.log("ERROR: Could not open todo database:",err); return false;}
+    try {usersDBAsAdmin = usersNanoAsAdmin.use("_users");}
+    catch(err) {console.log("ERROR: Could not open users database:", err); return false;}
     await addDBIdentifier();
     await checkAndCreateContent();
     await checkAndUpdateSchema();
@@ -531,7 +543,7 @@ async function checkUserByEmailExists(req, res) {
 
 async function issueToken(req, res) {
     const { username, password, deviceUUID } = req.body;
-    console.log("issuing token for device ID:", JSON.stringify(deviceUUID));
+    console.log("STATUS: issuing token for device ID:", JSON.stringify(deviceUUID));
     let response = {
         loginSuccessful: false,
         email: "",
@@ -558,7 +570,8 @@ async function issueToken(req, res) {
         userDoc.fullDoc.refreshJWTs = {};
      }
      userDoc.fullDoc.refreshJWTs[deviceUUID] = response.refreshJWT;
-     let userUpd = usersDBAsAdmin.insert(userDoc.fullDoc);
+     try {let userUpd = usersDBAsAdmin.insert(userDoc.fullDoc);}
+     catch(err) {console.log("ERROR: Could not update user: ",username,":",err); response.loginSuccessful=false;}
     return(response);
 
 }
@@ -652,7 +665,8 @@ async function refreshToken(req, res) {
     response.accessJWT = await generateJWT({username: tokenDecode.payload.sub, deviceUUID: deviceUUID, includeRoles: true, timeString: accessTokenExpires});
     let userResponse = await getUserDoc(tokenDecode.payload.sub);
     userResponse.fullDoc.refreshJWTs[deviceUUID] = response.refreshJWT;
-    let update = await usersDBAsAdmin.insert(userResponse.fullDoc);
+    try {let update = await usersDBAsAdmin.insert(userResponse.fullDoc);}
+    catch(err) {console.log("ERROR: Could not update user(refresh token):",err); response.valid=false;}
 //    console.log("about to return from refresh Token:",{status},{response});
     return ({status, response});
 }
@@ -700,9 +714,6 @@ async function logout(req, res) {
     let update = null;
     try { update = await usersDBAsAdmin.insert(userResponse.fullDoc); }
     catch(err) { console.log("ERROR: problem logging out user: ",err); }
-
-
-
 }
 
 async function authenticateJWT(req,res,next) {
@@ -725,7 +736,8 @@ async function updateUnregisteredFriends(email) {
         selector: { type: { "$eq": "friend" }, inviteEmail: { "$eq": email}},
         limit: await totalDocCount(todosDBAsAdmin)
     }
-    foundFriendDocs =  await todosDBAsAdmin.find(emailq);
+    try {foundFriendDocs =  await todosDBAsAdmin.find(emailq);}
+    catch(err) {console.log("ERROR: Could not find friend documents:",err); return false;}
     foundFriendDoc = undefined;
 //    if (foundFriendDocs.docs.length > 0) {foundFriendDoc = foundFriendDocs.docs[0]}
     foundFriendDocs.docs.forEach(async (doc) => {
@@ -828,7 +840,11 @@ async function createAccountUIGet(req, res) {
         fields: [ "friendID1","friendID2","inviteUUID","inviteEmail","friendStatus"],
         limit: await totalDocCount(todosDBAsAdmin)
     }
-    let foundFriendDocs =  await todosDBAsAdmin.find(uuidq);
+    let foundFriendDocs;
+    try {foundFriendDocs =  await todosDBAsAdmin.find(uuidq);}
+    catch(err) {console.log("ERROR: Could not find friend documents:",err);
+                respObj.formError="Database Error Encountered";
+                return respObj;}
     let foundFriendDoc;
     if (foundFriendDocs.docs.length > 0) {foundFriendDoc = foundFriendDocs.docs[0]}
     else {
@@ -853,7 +869,9 @@ async function getFriendDocByUUID(uuid) {
         selector: { type: { "$eq": "friend" }, inviteUUID: { "$eq": uuid}},
         limit: await totalDocCount(todosDBAsAdmin)
     }
-    let foundFriendDocs =  await todosDBAsAdmin.find(uuidq);
+    let foundFriendDocs;
+    try {foundFriendDocs =  await todosDBAsAdmin.find(uuidq);}
+    catch(err) {console.log("ERROR: Could not find friend documents:", err); return null;};
     let foundFriendDoc;
     if (foundFriendDocs.docs.length > 0) {foundFriendDoc = foundFriendDocs.docs[0]}
     return(foundFriendDoc);
@@ -926,7 +944,11 @@ async function createAccountUIPost(req,res) {
     const emailq = {
         selector: { type: { "$eq": "friend" }, inviteEmail: { "$eq": req.body.email}}
     }
-    foundFriendDocs =  await todosDBAsAdmin.find(emailq);
+    let foundFriendDocs;
+    try {foundFriendDocs =  await todosDBAsAdmin.find(emailq);}
+    catch(err) {console.log("ERROR: Could not find friend by email:",err); 
+                respObj.formError="Database error finding friend by email";
+                return respObj;}
     foundFriendDoc = undefined;
     if (foundFriendDocs.docs.length > 0) {foundFriendDoc = foundFriendDocs.docs[0]}
     foundFriendDocs.docs.forEach(async (doc) => {
@@ -1051,14 +1073,20 @@ async function resetPasswordUIPost(req, res) {
         let newDoc=cloneDeep(userDoc.fullDoc);
         newDoc.password=req.body.password;
         let newDocFiltered = _.pick(newDoc,['_id','_rev','name','email','fullname','roles','type','password','refreshJWTs']);
-        let docupdate = await usersDBAsAdmin.insert(newDocFiltered);
+        try {let docupdate = await usersDBAsAdmin.insert(newDocFiltered);}
+        catch(err) {console.log("ERROR: Couldn't update user/reset password:",err);
+                    respObj.formError="Database error resetting password";
+                    respObj.createdSuccessfully=false;
+                    return respObj;}
     }
     respObj.resetSuccessfully = true;
     return(respObj);
 }
 
 async function resolveConflicts() {
-    const conflicts = await todosDBAsAdmin.view(conflictsViewID,conflictsViewName);
+    let conflicts;
+    try {conflicts = await todosDBAsAdmin.view(conflictsViewID,conflictsViewName);}
+    catch(err) {console.log("ERROR: Couldn't create conflicts view:",err); return false;}
     console.log("STATUS: Resolving all conflicts started...");
     let resolveFailure=false;
     if (conflicts.rows?.length <= 0) {console.log("STATUS: no conflicts found"); return};
@@ -1066,7 +1094,7 @@ async function resolveConflicts() {
         const conflict = conflicts.rows[i];
         let curWinner;
         try { curWinner = await todosDBAsAdmin.get(conflict.id, {conflicts: true});}
-        catch(err) { resolveFailure = true;}
+        catch(err) { console.log("ERROR: Error resolving conflicts:",err); resolveFailure = true;}
         if (resolveFailure) {continue};
         let latestDocTime = curWinner.updatedAt; 
         let latestIsCurrentWinner = true;
@@ -1077,7 +1105,7 @@ async function resolveConflicts() {
             const losingRev = curWinner._conflicts[j];
             let curLoser;
             try { curLoser = await todosDBAsAdmin.get(conflict.id,{ rev: losingRev})}
-            catch(err) {resolveFailure=true;}
+            catch(err) {console.log("ERROR: Error resolving conflicts:",err); resolveFailure=true;}
             if (resolveFailure) {continue outerloop};
             if (curLoser.updatedAt >= latestDocTime) {
                 latestIsCurrentWinner = false;
@@ -1109,7 +1137,9 @@ async function resolveConflicts() {
 
 async function expireJWTs() {
     console.log("STATUS: Checking for expired JWTs");
-    let userDocs = await usersDBAsAdmin.list({include_docs: true});
+    let userDocs;
+    try {userDocs = await usersDBAsAdmin.list({include_docs: true});}
+    catch(err) {console.log("ERROR: Could not find user records to expire JWTs:",err); return false;}
     for (let i = 0; i < userDocs.rows.length; i++) {
         const userDoc = userDocs.rows[i].doc;
         if (userDoc.hasOwnProperty("refreshJWTs")) {
