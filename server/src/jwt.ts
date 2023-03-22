de192e85a2330f35a0ece4368b1fe60a0227e2f8
David Shay <davideshay@gmail.com>
2023-03-22 18:18:52 -0400

    WIP: backend typescript fixes

--- server/src/jwt.ts
+++ server/src/jwt.ts
@@ -1,17 +1,36 @@
-import jose from 'jose';
+import jose, { JWTPayload } from 'jose';
 import { usersDBAsAdmin } from './dbstartup';
 import { couchKey, couchStandardRole } from './apicalls';
 import { getUserDoc } from './utilities';
 import { isEqual, isEmpty } from 'lodash';
+import {UserDoc} from './DBSchema'
+import { DocumentListResponse, MangoResponse } from 'nano';
 
 const JWTKey = new TextEncoder().encode(couchKey);
 
-export async function isValidToken(refreshJWT) {
+export type TokenPayload = {
+    sub: string,
+    deviceUUID: string
+}
+
+export const TokenPayloadInit: TokenPayload = {
+    sub: "",
+    deviceUUID: ""
+}
+
+export type TokenReturnType = {
+    isValid: boolean,
+    protectedHeader: null | string,
+    payload: null | JWTPayload,
+    error: any
+}
+
+export async function isValidToken(refreshJWT: string) {
     let jwtResponse = null;
-    let returnValue = {
+    let returnValue: TokenReturnType = {
         isValid: false,
         protectedHeader: null,
-        payload: null,
+        payload: TokenPayloadInit,
         error : null
     };
     try { jwtResponse = await jose.jwtVerify(refreshJWT, JWTKey); }
@@ -20,17 +39,17 @@ export async function isValidToken(refreshJWT) {
     if (jwtResponse.hasOwnProperty("payload") && jwtResponse.hasOwnProperty("protectedHeader")) {
         if (jwtResponse.payload.hasOwnProperty("sub")) {
             returnValue.isValid = true;
-            returnValue.protectedHeader = jwtResponse.protectedHeader;
-            returnValue.payload = jwtResponse.payload;
+//            returnValue.protectedHeader = jwtResponse.protectedHeader;
+            returnValue.payload = jwtResponse.payload as TokenPayload;
         }
     }        
     return returnValue;
 }
 
-export async function generateJWT({ username, deviceUUID, timeString, includeRoles}) {
+export async function generateJWT ({ username, deviceUUID, timeString, includeRoles}: {username: string, deviceUUID: string, timeString: string, includeRoles: boolean}) {
     const alg = "HS256";
     const secret = new TextEncoder().encode(couchKey);
-    const payload = {'sub': username, 'deviceUUID': deviceUUID};
+    const payload: JWTPayload = {'sub': username, 'deviceUUID': deviceUUID};
     if (includeRoles) { 
         payload["_couchdb.roles"] =  [couchStandardRole];
         payload["tokenType"] = "access"
@@ -45,7 +64,7 @@ export async function generateJWT({ username, deviceUUID, timeString, includeRol
     return (jwt);
 }
 
-export async function JWTMatchesUserDB(refreshJWT, deviceUUID, username) {
+export async function JWTMatchesUserDB(refreshJWT: string, deviceUUID: string, username: string) {
     let userDoc: any = await getUserDoc(username);
     if (userDoc.fullDoc.hasOwnProperty('refreshJWTs')) {
         console.log("Refresh JWT matches the database:",userDoc.fullDoc.refreshJWTs[deviceUUID] == refreshJWT);    
@@ -57,7 +76,7 @@ export async function JWTMatchesUserDB(refreshJWT, deviceUUID, username) {
     return true;
 }
 
-export async function invalidateToken(username, deviceUUID, invalidateAll) {
+export async function invalidateToken(username: string, deviceUUID: string, invalidateAll: boolean) {
     console.log("invalidating token now...");
     let userDoc: any = await getUserDoc(username);
     if (userDoc.error) { return false;}
@@ -76,16 +95,17 @@ export async function invalidateToken(username, deviceUUID, invalidateAll) {
 
 export async function expireJWTs() {
     console.log("STATUS: Checking for expired JWTs");
-    let userDocs;
-    try {userDocs = await usersDBAsAdmin.list({include_docs: true});}
+    let res: DocumentListResponse<unknown> | null = null;
+    try {res = (await usersDBAsAdmin.list({include_docs: true})) ;}
     catch(err) {console.log("ERROR: Could not find user records to expire JWTs:",err); return false;}
-    for (let i = 0; i < userDocs.rows.length; i++) {
-        const userDoc = userDocs.rows[i].doc;
+    if (res == null || !res.hasOwnProperty("rows")) { console.log("ERROR: No user rows found"); return false;}
+    for (let i = 0; i < res.rows.length; i++) {
+        const userDoc: UserDoc = (res.rows[i].doc as UserDoc);
         if (userDoc.hasOwnProperty("refreshJWTs")) {
-            let updateJWTs = {};
+            let updateJWTs: any = {};
             for (const [device,jwt] of Object.entries(userDoc.refreshJWTs)) {
                 if (isEmpty(jwt)) {continue;}
-                let jwtVerify = await isValidToken(jwt);
+                let jwtVerify = await isValidToken(String(jwt));
                 if (jwtVerify.isValid) {updateJWTs[device] = jwt}
             }
             if (!isEqual(updateJWTs,userDoc.refreshJWTs)) {
