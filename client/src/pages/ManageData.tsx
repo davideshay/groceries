a6af2e52f469d045c6951a0ec829ac35300f4e85
David Shay <davideshay@gmail.com>
2024-12-23 02:33:30 -0500

    compile fixes

--- client/src/pages/ManageData.tsx
+++ client/src/pages/ManageData.tsx
@@ -1,33 +1,16 @@
 import { IonContent,  IonPage, IonList, IonItem } from '@ionic/react';
 import { useRef } from 'react';
-import { useItems } from '../components/Usehooks';
-import { HistoryProps, RowType} from '../components/DataTypes';
-import ErrorPage from './ErrorPage';
-import { Loading } from '../components/Loading';
+import { HistoryProps } from '../components/DataTypes';
 import PageHeader from '../components/PageHeader';
 import { useTranslation } from 'react-i18next';
-import { translatedItemName } from '../components/translationUtilities';
 
 // The AllItems component is a master editor of all of the known items in the database.
 // Each item has a name, along with data about each list the item is on (list ID, quantity, count of number of times bought,
 // and status for active (on the list), and complete (on the list and checked off) )
 
 const ManageData: React.FC<HistoryProps> = (props: HistoryProps) => {
-  const { dbError: itemError,  itemRowsLoaded, itemRows} = useItems({selectedListGroupID: null, isReady :true, needListGroupID: false, activeOnly: false, selectedListID: null, selectedListType: RowType.list});
   const screenLoading = useRef(true);
   const { t } = useTranslation();
-
-  if  (itemError) { return (
-    <ErrorPage errorText={t("error.loading_item_info_restart") as string} ></ErrorPage>
-    )}
-
-  if (itemRowsLoaded ) {
-    screenLoading.current = false;
-  } else {
-    screenLoading.current = true;
-    return ( <Loading isOpen={screenLoading.current} message={t("general.loading_all_items")} />
-//    setIsOpen={() => {screenLoading.current = false}} />
-  )}
   
   screenLoading.current = false;
 
