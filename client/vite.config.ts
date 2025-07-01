561769c0378325608e26d36d1344aa7f0b338ce1
David Shay <davideshay@gmail.com>
2025-06-30 20:29:43 -0400

    Update package dependencies

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from 'vite-plugin-pwa';
+// import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -28,5 +28,7 @@ export default defineConfig({
       }
     }},
   },
-  plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
+  plugins: [react(), 
+    // VitePWA({ registerType: 'autoUpdate' })
+  ],
 });
